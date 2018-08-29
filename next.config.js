86106aeefc89d3a0febfffaae2d4fa204c19a186
mgechev <mgechev@gmail.com>
2018-08-28 18:37:33 -0700

    Remove period

--- next.config.js
+++ next.config.js
@@ -3,10 +3,6 @@ const { GuessPlugin } = require('guess-webpack')
 module.exports = {
   webpack: function (config, { isServer }) {
     config.plugins.push(new GuessPlugin({
-      period: {
-        startDate: new Date('5-10-2018'),
-        endDate: new Date()
-      },
       reportProvider() {
         return Promise.resolve(JSON.parse(require('fs').readFileSync('./routes.json')));
       },
