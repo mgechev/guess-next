908afd244ac3c692e047c8d5b4457f0478995516
mgechev <mgechev@gmail.com>
2018-08-30 15:07:00 -0700

    Update next.config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const { GuessPlugin } = require('guess-webpack')
 
 module.exports = {
   webpack: function (config, { isServer }) {
+    if (isServer) return config;
     config.plugins.push(new GuessPlugin({
       reportProvider() {
         return Promise.resolve(JSON.parse(require('fs').readFileSync('./routes.json')));
